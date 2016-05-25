84a0faf6e212d0343c436a24d001c2c801bfe639
Alex Soble <asoble@gmail.com>
2016-05-24 20:23:30 -0500

    Use Jekyll to generate a _site folder

---
+++ _site/helpers.js
@@ -0,0 +1,22 @@
+function makeWordShorterMaybe (word, count) {
+  if (count > 2) {
+    return word;
+  } else {
+    var words = word.split(' ');
+    return [words[0], words[1]].join(' ');
+  }
+};
+
+function makeBubble (text, size) {
+  var label = makeWordShorterMaybe(text, size);
+  var randomColor = d3Colors[Math.floor(Math.random() * d3Colors.length)];
+  var growthFactor = String(Math.sqrt(size) * 50);
+
+  var label = "<div class='label-text'>" + label + "</div>";
+
+  var bubble = "<div class='bubble' style='width: " + growthFactor + "px;" +
+                      "height: " + growthFactor + "px;" +
+                      "background-color: " + randomColor + "'></div>";
+
+  return "<div class='bubble-wrapper'>" + label + bubble + "</div>";
+};
